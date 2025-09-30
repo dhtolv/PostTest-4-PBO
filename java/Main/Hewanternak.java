02fdf31a113acba9969ffbf5658d0af0e3a2f90f
d <dhtolv0610@gmail.com>
2025-09-30 20:58:09 +0800

    Add files via upload

---
+++ java/Main/Hewanternak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author Asus
+ */
+public class Hewanternak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
